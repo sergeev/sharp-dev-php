a6f70158e5f191c414f43043d5d2c00b8827c587
sergeev <v.sergeev.m@gmail.com>
2018-12-14 20:41:56 +0700

    Base auth add

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -59,10 +59,10 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'port' => env('DB_PORT', '5433'),
+            'database' => env('DB_DATABASE', 'sharp_dev_php'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', '10184'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
